6d01907df6d3191ef69e9e7dd15c2ac9d0032b98
Noora Virolainen <vino@cubbli18-gold.local>
2019-11-05 09:02:07 +0200

    .gitignore

--- NhlStatistics1/src/test/java/ohtuesimerkki/StatisticksTest.java
+++ NhlStatistics1/src/test/java/ohtuesimerkki/StatisticksTest.java
@@ -5,7 +5,6 @@ package ohtuesimerkki;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import java.util.ArrayList;
 import java.util.List;
 import ohtuesimerkki.Player;
@@ -22,66 +21,58 @@ import static org.junit.Assert.*;
  * @author vino
  */
 public class StatisticksTest {
-    
+
     Reader readerStub = new Reader() {
- 
+
         public List<Player> getPlayers() {
             ArrayList<Player> players = new ArrayList<>();
- 
+
             players.add(new Player("Semenko", "EDM", 4, 12));
             players.add(new Player("Lemieux", "PIT", 45, 54));
-            players.add(new Player("Kurri",   "EDM", 37, 53));
+            players.add(new Player("Kurri", "EDM", 37, 53));
             players.add(new Player("Yzerman", "DET", 42, 56));
             players.add(new Player("Gretzky", "EDM", 35, 89));
-            
- 
+
             return players;
         }
     };
- 
+
     Statistics stats;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         // luodaan Statistics-olio joka käyttää "stubia"
         stats = new Statistics(readerStub);
-    }  
+    }
 
-    
-   
-    
     @AfterClass
     public static void tearDownClass() {
     }
-    
- 
-    
+
     @After
     public void tearDown() {
     }
 
+    @Test
+    public void search() {
+        assertEquals("Semenko", stats.search("Semenko").getName());
 
-     @Test
-     public void search() {
-         
-       assertEquals("Semenko", stats.search("Semenko").getName());
-         
-     }
-     @Test
-     public void search2() {   
-       assertEquals(null, stats.search("ir"));
-         
-     }
-     
-     @Test
-     public void topS() {     
-      assertEquals("Lemieux", stats.topScorers(4).get(1).getName());     
-     }
-        @Test
-     public void Team() {     
-      assertEquals("Lemieux", stats.team("PIT").get(0).getName());     
-     }
-     
-     
-     
+    }
+
+    @Test
+    public void search2() {
+        assertEquals(null, stats.search("ir"));
+
+    }
+
+    @Test
+    public void topS() {
+        assertEquals("Lemieux", stats.topScorers(4).get(1).getName());
+    }
+
+    @Test
+    public void Team() {
+        assertEquals("Lemieux", stats.team("PIT").get(0).getName());
+    }
+
 }
