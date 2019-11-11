adea3956cc12af83cc4ee583b22153e3954137bb
Noora Virolainen <vino@cubbli18-gold.local>
2019-11-11 15:22:00 +0200

    tehtävä 9

--- Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -2,16 +2,16 @@ package ohtu.verkkokauppa;
 
 public class Kauppa {
 
-    private Varasto varasto;
-    private Pankki pankki;
+    private RajapintaVarastolle varasto;
+    private RajapintaPankille pankki;
     private Ostoskori ostoskori;
-    private Viitegeneraattori viitegeneraattori;
+    private RajapintaViitteelle viitegeneraattori;
     private String kaupanTili;
 
-    public Kauppa() {
-        varasto = Varasto.getInstance();
-        pankki = Pankki.getInstance();
-        viitegeneraattori = Viitegeneraattori.getInstance();
+    public Kauppa(RajapintaVarastolle varasto, RajapintaPankille pankki, RajapintaViitteelle viitegeneraattori) {
+       this.varasto = varasto;
+        this.pankki = pankki;
+        this.viitegeneraattori = viitegeneraattori;
         kaupanTili = "33333-44455";
     }
 
